b569f20d2a26e4170016de27c75716f2c8c8f292
Gracie-lab <gracebolutife@gmail.com>
2021-05-19 12:52:31 +0100

    setup

---
+++ web/src/main/java/com/bukkaspot/BukkaspotApplication.java
@@ -0,0 +1,13 @@
+package com.bukkaspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukkaspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukkaspotApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/bukkaspot/BukkaspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bukkaspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukkaspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
